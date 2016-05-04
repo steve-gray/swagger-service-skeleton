efbf967e6d610526966ed8093bc739326b792553
steven.gray <steve@eventualconsistency.net>
2016-05-05 06:55:06 +1000

    More IoC Testing.

--- tests/controllers/math.js
+++ tests/controllers/math.js
@@ -1,6 +1,15 @@
 'use strict';
 
 class MathController {
+  constructor(req, res) {
+    if (!req) {
+      throw new Error('No req - check ioc');
+    }
+    if (!res) {
+      throw new Error('No res - check ioc');
+    }
+  }
+
   add(x, y, responder) {
     if (x <= 0 || y <= 0) {
       throw new Error('We only tolerate positive integers in this school');
