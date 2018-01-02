226e52179ed27495794caec5b1bbdf9b6391faab
Sean <fastbeanau@gmail.com>
2018-01-02 17:13:21 +1000

    Improve swagger param error handling

--- src/middleware/swagger-error-handler.js
+++ src/middleware/swagger-error-handler.js
@@ -5,11 +5,19 @@ const debug = require('debug')('swagger-service-skeleton:swagger-error-handler')
 module.exports = () =>
   (error, req, res, next) => {
     if (error.failedValidation) {
-      res.json({
-        message: `Parameter (${error.paramName}) validation failed`,
-        errors: error.results.errors,
-        warnings: error.results.warnings,
-      }, 400);
+      res.json(
+        Object.prototype.hasOwnProperty.call(error, 'results') ?
+          {
+            message: error.message,
+            errors: error.results.errors,
+            warnings: error.results.warnings,
+          } :
+          {
+            message: error.message,
+            errors: [{ code: error.code, message: error.message, path: error.path }],
+            warnings: [],
+          }
+        , 400);
     }
     else {
       debug('ERROR Occurred: %s', error);
